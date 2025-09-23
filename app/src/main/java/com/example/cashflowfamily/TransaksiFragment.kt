c456bb931db018c0e81e3126f3d21546a753f53c
MarvellCe <marvellwij@gmail.com>
2025-09-24 01:57:47 +0700

    Transaksi

--- app/src/main/java/com/example/cashflowfamily/TransaksiFragment.kt
+++ app/src/main/java/com/example/cashflowfamily/TransaksiFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class TransaksiFragment : Fragment() {
 
@@ -14,13 +16,28 @@ class TransaksiFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_transaksi, container, false)
+        return inflater.inflate(R.layout.fragment_transaksi, container, false)
+    }
 
-        val btnTambah = view.findViewById<Button>(R.id.btnTambahTransaksi)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val rvTransaksi: RecyclerView = view.findViewById(R.id.rv_transaksi)
+        rvTransaksi.layoutManager = LinearLayoutManager(context)
+        rvTransaksi.adapter = TransaksiAdapter(createDummyFamilyTransaksi())
+
+        val btnTambah: Button = view.findViewById(R.id.btnTambahTransaksi)
         btnTambah.setOnClickListener {
             findNavController().navigate(R.id.action_transaksiFragment_to_formTransaksiFragment)
         }
+    }
 
-        return view
+    private fun createDummyFamilyTransaksi(): List<Transaksi> {
+        return listOf(
+            Transaksi("Gaji Ayah", "23 Sep 2025", 7500000.0),
+            Transaksi("Bayar SPP Sekolah Anak", "22 Sep 2025", -550000.0),
+            Transaksi("Belanja Bulanan Supermarket", "21 Sep 2025", -1200000.0),
+            Transaksi("Uang Saku Mingguan Anak", "20 Sep 2025", -150000.0)
+        )
     }
 }
